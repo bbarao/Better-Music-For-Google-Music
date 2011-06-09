ce718f4ed9096b7b44b03ccc79305be8e2fe41fa
Jared Sartin <jaredsartin@gmail.com>
2011-06-09 10:49:47 -0400

    Working scrobbling, popup with styles, no play controls yet

--- BetterMusicBeta/js/background.js
+++ BetterMusicBeta/js/background.js
@@ -2,44 +2,35 @@
  * background.js
  * Controls the data transfer between page, bacground page, and last.fm
  * @author Jared Sartin <jared@level2studios.com>
- * @author Derek Slenk <derek@level2studios.com>
+ * Based on work by @author Alexey Savartsov <asavartsov@gmail.com>
  * Licensed under the MIT license
  */
 
-var CurrentSongInfo = '';
-var lastfm_api_key = '41b5091ff76ed125105e038191937bd9';
-var lastfm_secret_key = '4a4e7fc6803dc19275deaa672c70627f';
-var lastfm_username = '';
-var lastfm_password = '';
 
-var lastfm_session;
-
-var cache = new LastFMCache();
-
-var lastfm = new LastFM({
-  apiKey    : lastfm_api_key,
-  apiSecret : lastfm_secret_key,
-  cache     : cache
-});
-
-// to store use 
-// http://www.smashingmagazine.com/2010/10/11/local-storage-and-how-to-use-it/
-// http://diveintohtml5.org/storage.html
-
-//EXAMPLES:
-
-//var foo = localStorage.getItem("bar");
-// ...
-//localStorage.setItem("bar", foo);
-
-//OR
-
-//var foo = localStorage["bar"];
-// ...
-//localStorage["bar"] = foo;
+var SETTINGS = {
+    api_key: "ae40619c4838789cf6660391be7b6ada",
+    api_secret: "ee955e252af3c85e66e15864e31174fe",
+    
+    callback_file: "lastfm_callback.html",
+};
 
 var player = {}; // Previous player state
-var currentSong = '';
+var now_playing_sent = false;
+var scrobbled = false;
+var lastfm_api = new LastFM(SETTINGS.api_key, SETTINGS.api_secret);
+
+var currentSong = ''; //var CurrentSongInfo = '';
+
+// Load settings from local storage
+lastfm_api.session.key = localStorage["session_key"] || null;
+lastfm_api.session.name = localStorage["session_name"] || null;
+
+// This enables scrobbling by default
+SETTINGS.scrobble = !(localStorage["scrobble"] == "false");
+
+if(!SETTINGS.scrobble) {
+    chrome.browserAction.setIcon({ 'path': SETTINGS.scrobbling_stopped_icon });
+}
 
 // Connect event handlers
 chrome.extension.onConnect.addListener(port_on_connect);
@@ -48,7 +39,8 @@ chrome.extension.onConnect.addListener(port_on_connect);
  * Content script has connected to the extension
  */
 function port_on_connect(port) {
-    console.assert(port.name == "musicbeta");
+    console.assert(port.name == "musicbeta"); 
+
     // Connect another port event handlers
     port.onMessage.addListener(port_on_message);
     port.onDisconnect.addListener(port_on_disconnect);
@@ -56,7 +48,7 @@ function port_on_connect(port) {
  
 /**
  * New message arrives to the port
- **/
+ */
 function port_on_message(message) {
     // Current player state
     var _p = message;
@@ -66,18 +58,62 @@ function port_on_message(message) {
       currentSong = _p.song.title + _p.song.artist;
       need_to_toast = true;
     }
-
+    
+    if(!SETTINGS.scrobble) {
+        player = _p;
+        return;
+    }
+    
     if(_p.has_song) {
         if(_p.is_playing) {
             chrome.browserAction.setIcon({'path': 'img/main-play.png' });
             
-            //This is the scrobble area
+            // Last.fm recommends to scrobble a song at least at 50%
+            var time_to_scrobble = _p.song.time * 0.7 - _p.song.position;
             
+            // Check for valid timings and for that the now playing status was reported at least once
+            // This intended to fix an issue with invalid timings that Amazon accidentally reports on
+            // song start
+            if(time_to_scrobble <= 0 && _p.song.position > 0 && _p.song.time > 0) {
+                if(now_playing_sent && !scrobbled) {
+                    // Scrobble this song
+                    lastfm_api.scrobble(_p.song.title,
+                        /* Song start time */
+                        Math.round(new Date().getTime() / 1000) - _p.song.position, 
+                        _p.song.artist,
+                        _p.song.album,
+                        function(response) {
+                            if(!response.error) {
+                              // Song was scrobled, waiting for the next song
+                                scrobbled = true;
+                                now_playing_sent = false;
+                            }
+                            else {
+                                if(response.error == 9) {
+                                    // Session expired
+                                    clear_session();
+                                }
+                            }
+                        });
+                }
+            }
+            else {
+                // Set now playing status
+                lastfm_api.now_playing(_p.song.title,
+                    _p.song.artist,
+                    _p.song.album,
+                    function(response) {
+                    });
+                
+                now_playing_sent = true;
+                scrobbled = false;
+            }
+
             if (need_to_toast){
               ToastyPopup(_p.song.cover, _p.song.title, _p.song.artist);
               need_to_toast = false;
             }
-
+             
             // Save player state
             player = _p;
         }
@@ -89,6 +125,8 @@ function port_on_message(message) {
     else {
         chrome.browserAction.setIcon({ 'path': 'img/main-idle.png' });
         player = {};
+        scrobbled = false;
+        now_playing_sent = false;
         need_to_toast = true;
         currentSong = '';
     }
@@ -99,14 +137,59 @@ function port_on_message(message) {
   */
 function port_on_disconnect() {
     player = {}; // Clear player state
-    now_playing_toast = false;
+    scrobbled = false;
+    now_playing_sent = false;
+    chrome.browserAction.setIcon({ 'path': 'img/main-idle.png' });
+}
+
+
+/**
+ * Authentication link from popup window
+ */
+function start_web_auth() {
+    var callback_url = chrome.extension.getURL(SETTINGS.callback_file);
+    chrome.tabs.create({
+        'url': 
+            "http://www.last.fm/api/auth?api_key=" + SETTINGS.api_key + "&cb=" + callback_url });
+}
+
+/**
+ * Clears last.fm session
+ */
+function clear_session() {
+    lastfm_api.session = {};
+    
+    localStorage.removeItem("session_key");
+    localStorage.removeItem("session_name");
+}
+
+/**
+ * Toggles setting to scrobble songs or not
+ */
+function toggle_scrobble() {
+    SETTINGS.scrobble = !SETTINGS.scrobble;
+    localStorage["scrobble"] = SETTINGS.scrobble;
+}
+
+/**
+ * Last.fm session request
+ */
+function get_lastfm_session(token) {
+    lastfm_api.authorize(token, function(response) {
+        // Save session
+        if(response.session)
+        {
+            localStorage["session_key"] = response.session.key;
+            localStorage["session_name"] = response.session.name;
+        }
+    });
 }
 
  /**
   * Sends the information to the desktop toast
   */
 function ToastyPopup(icon, title, artist){
-  //TrackToast();
+  TrackUse('Toast');
   // Create a simple text notification:
   if(icon == "http:default_album_med.png"){
     icon = "img/logo-48x48.png";
@@ -128,6 +211,6 @@ function ToastyPopup(icon, title, artist){
  /**
   * Send event to GA
   */
-function TrackToast(){
-  _gaq.push(['_trackEvent', 'MusicToast', 'Toast']);
+function TrackUse(eventName){
+  _gaq.push(['_trackEvent', 'MusicToast', eventName]);
 }
--- readme.md
+++ readme.md
@@ -34,3 +34,16 @@ If you like what we are doing, fee free to buy us a beer or help us out so we ca
 **1.0.1** Fixed the order of artist/song title, updated icons with HQ Google Music icon
 
 **1.0.0** Initial version, basic toasting of song information on change
+
+* * *
+
+### License
+(The MIT License)
+
+Copyright (c) 2011 Alexey Savartsov asavartsov@gmail.com, Brad Lambeth brad@lambeth.us, Jared Sartin jared@level2studios.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
