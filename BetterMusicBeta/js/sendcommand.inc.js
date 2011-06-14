e0b95f2b300249087f655fb1b35d3db663d3632a
Jared Sartin <jaredsartin@gmail.com>
2011-06-14 08:25:36 -0400

    Split sendCommand into own JS to allow include in all html pages and keep system DRY

---
+++ BetterMusicBeta/js/sendcommand.inc.js
@@ -0,0 +1,55 @@
+/**
+ * sendcommand.inc.js
+ * Send command code - keeping the system DRY
+ * @author Jared Sartin <jared@level2studios.com>
+ * Licensed under the MIT license
+ */
+
+//Find the Google Music Beta tab
+function FindMusicBetaTab(callback) {
+chrome.windows.getAll({populate: true}, function(windows) {
+    for (var window = 0; window < windows.length; window++) {
+      for (var i = 0; i < windows[window].tabs.length; i++) {
+        if (windows[window].tabs[i].url.
+            indexOf('http://music.google.com/music/listen') == 0 ||
+            windows[window].tabs[i].url.
+            indexOf('https://music.google.com/music/listen') == 0) {
+          callback(windows[window].tabs[i].id)
+          return;
+        }
+      }
+    }
+    callback(null);
+  });  
+}
+
+// Send the given command to a tab showing Music Beta,
+// or open one if non exists.
+function sendCommand(command) {
+    FindMusicBetaTab(function(tab_id) {
+        if (tab_id) {
+          if (command == "foreground") {
+            chrome.tabs.update(tab_id, {selected: true});
+          } else if (command == "ratingUp") {
+            chrome.tabs.executeScript(tab_id,
+                {
+                  code: "location.assign('javascript:thumbsUp();void 0');",
+                  allFrames: true
+                });
+          } else {
+            chrome.tabs.executeScript(tab_id,
+                {
+                  code: "location.assign('javascript:SJBpost(\"" + command +
+                        "\");void 0');",
+                  allFrames: true
+                });
+			if(command == 'nextSong' || command == 'prevSong'){
+				notification_close();
+			}
+          }
+        } else {
+          chrome.tabs.create({url: 'http://music.google.com/music/listen',
+                              selected: true});
+        }
+    });
+}
